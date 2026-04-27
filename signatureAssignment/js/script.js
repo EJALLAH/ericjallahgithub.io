cc2f58b4e1540cf4f7b7285568683dd5d983886e
EJALLAH <JALLAEF23@juniata.edu>
2026-04-27 12:30:30 -0400

    update

--- signatureAssignment/js/script.js
+++ signatureAssignment/js/script.js
@@ -1,9 +1,11 @@
 function showMessage() {
+    // Grabs the value from the input field
     let name = document.getElementById("nameInput").value;
 
+    // Checks if the user actually typed something
     if (name === "") {
         document.getElementById("welcomeText").innerHTML =
-        "Welcome! Enter your name in the order form.";
+        "Welcome! Enter your name in the box.";
     } else {
         document.getElementById("welcomeText").innerHTML =
         "Welcome " + name + "! Thanks for visiting our pizza palace.";
